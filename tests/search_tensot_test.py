f12e051f9b26d0050f6c455ac407c5f6c683d20a
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-03 19:46:41 +0400

    added configuration py file for initialize browser from config

--- tests/search_tensot_test.py
+++
@@ -1,9 +0,0 @@
-import json
-import pytest
-
-
-@pytest.fixture(scope='session')
-def config():
-    with open("tests/config.json") as config_file:
-        data = json.load(config_file)
-        return data
